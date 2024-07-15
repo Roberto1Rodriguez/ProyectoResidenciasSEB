748154a6bbd5ee21270f389dd7e9d994110a17fb
belen-ru <belenruiz911@outlook.com>
2024-07-15 02:16:47 -0600

    agg admin index

---
+++ ProyectoResidenciasWeb/Pages/AdministradorIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoResidenciasWeb.Pages
+{
+    public class AdministradorIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
