a2b47ae51406c3e38a1b0c186ff30a654c63d892
belen-ru <belenruiz911@outlook.com>
2024-07-15 19:57:32 -0600

    mas cambios, agg alumno, agg docente

---
+++ ProyectoResidenciasWeb/Pages/AgregarDocente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoResidenciasWeb.Pages
+{
+    public class AgregarDocenteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ProyectoResidenciasWeb/wwwroot/js/script.js
+++ ProyectoResidenciasWeb/wwwroot/js/script.js
@@ -45,3 +45,5 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
